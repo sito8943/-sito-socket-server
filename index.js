446422b0dfc5294604f01fd474b57c46343420c6
Sito8943 <sito8943@gmail.com>
2024-08-31 11:34:42 -0400

    added docs and formatting for socket server

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 import { Server } from "socket.io";
 
-class SocketServer {
-  prepareClient = (socket) => {};
+/**
+ * @description Socket Server
+ */
+export class SocketServer {
+  /**
+   * Prepare client socket with custom events
+   * @param {object} socket
+   */
+  prepareClient = (socket) => {
+    this.clients[socket.id] = socket;
+
+    socket.on("disconnect", (reason) => {
+      // * Whenever a client disconnects
+      if (this.clients[socket.id])
+        console.info(socket.id, "has disconnected. Reason:", reason);
+    });
+  };
 
   constructor(httpServer) {
-    this.clients = [];
+    this.clients = {};
+
     this.io = new Server(httpServer, {
       // options
     });
-    this.io.on("connection", (socket) => {});
-    this.io.on("disconnection", (socket) => {});
-    this.io.on("notify", (socket) => {});
+
+    // * Whenever a client connects
+    this.io.on("connection", (socket) => {
+      console.info("client connected", socket.id);
+
+      this.prepareClient(socket);
+    });
+
+    // * Whenever a client needs to be notified
+    this.io.on("notify", (notification) => {
+      if (this.clients[notification.target])
+        this.clients[notification.target].emit("notify", notification);
+    });
   }
 }
-
-export { SocketServer };
